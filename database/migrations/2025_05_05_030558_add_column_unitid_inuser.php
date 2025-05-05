33dbf27d03f47e1221c6949a575d518a9f046855
feeldacy <husnafelisacahyani@gmail.com>
2025-05-05 10:18:08 +0700

    added unit id column in user

---
+++ database/migrations/2025_05_05_030558_add_column_unitid_inuser.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function(Blueprint $table){
+            $table->string('unit_id')->nullable()->after('id');
+
+            $table->foreign('unit_id')->references('id')->on('unit_posyandu')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
